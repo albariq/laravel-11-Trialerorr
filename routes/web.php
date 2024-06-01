4a46186a3cce67d15c1b0061313c0577f3c77dd0
albariq <albariq00@gmail.com>
2024-06-01 11:50:31 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Route;
+use App\Models\Post;
+
+
+Route::get('/', function () {
+    return view('home', ['title' => 'Home Page']);
+});
+
+Route::get('/about', function () {
+    return view('about', ['name' => 'Al', 'title' => 'About']);
+});
+
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
+});
+
+Route::get('/posts/{post:slug}', function (Post $post) {
+
+    return view('post' , ['title' => 'Single Post', 'post' => $post]);
+});
+
+Route::get('/contact', function () {
+    return view('contact', ['title' => 'Contact']);
+});
+
+
+
